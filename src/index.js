22b0ccef4588fca4f9e1179f9d7ed4b818dc7cc1
Hector <hector@pennebaker.com>
2019-12-17 11:11:02 -0600

    First commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 // Test import of a JavaScript function, an SVG, and Sass
-import HelloWorld from './js/HelloWorld'
-import WebpackLogo from './images/webpack-logo.svg'
+// import HelloWorld from './js/HelloWorld'
+import heroImage from './images/hero-1.png'
+import heroImage2 from './images/hero-2.png'
+import cta1Image from './images/cta-texture.png'
+import recipe1Image from './images/recipe-1.png'
+import recipe2Image from './images/recipe-2.png'
+import recipe3Image from './images/recipe-3.png'
+import getRecipeBtn from './images/btn-get-recipe.png'
+import submitRecipe from './images/btn-submit-recipe.png'
 import './styles/index.scss'
 
 // Create SVG logo node
-const logo = document.createElement('img')
-logo.src = WebpackLogo
+// const logo = document.createElement('img')
+// logo.src = WebpackLogo
 
-// Create heading node
-const greeting = document.createElement('h1')
-greeting.textContent = HelloWorld()
+// // Create heading node
+// const greeting = document.createElement('h1')
+// greeting.textContent = HelloWorld()
 
 // Append SVG and heading nodes to the DOM
-const app = document.querySelector('#root')
-app.append(logo, greeting)
+// const app = document.querySelector('#root')
+// const heroContainer = document.querySelector('#heroContainer')
+// heroContainer.append(heroImage)
+// app.append(logo, greeting)
